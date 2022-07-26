4cb230fb0eb6bd799534efb9549e180bf51a23cb
tobi <noggnzzz@gmail.com>
2022-07-26 04:09:48 -0500

    Provincias del Perú

--- dirs/provincias_peru/JS/mijs/main.js
+++ dirs/provincias_peru/JS/mijs/main.js
@@ -15,38 +15,32 @@ var baseLayers = {
     'EsriWorldImagery': L.tileLayer.provider('Esri.WorldImagery')
 };
 
-// var geojsonMarkerOptions = {
-//     radius: 5,
-//     fillColor: "#4444f5a3",
-//     color: "#3388ff",
-//     weight: 1,
-//     opacity: 1,
-//     fillOpacity: 0.8
-// };
+var geojsonMarkerOptions = {
+    fillColor: "#4444f54a",
+    color: "#3388ffab",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
 
-// var anotherLayer = L.geoJSON(data, geojsonMarkerOptions).addTo(map);
-// var capitales = L.geoJson(data, {
-//     pointToLayer: function (feature, latlng) {
-//         return L.circleMarker(latlng, geojsonMarkerOptions);
-//     }
-// }).addTo(map);
+var anotherLayer = L.geoJSON(data, geojsonMarkerOptions).addTo(map);
 
 // Get legend url format
 // http://idesep.senamhi.gob.pe:80/geoserver/g_03_04/wms?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=g_03_04:03_04_001_03_001_513_0000_00_00
 
 
-var layer001 = L.tileLayer.wms('https://idesep.senamhi.gob.pe/geoserver/g_03_04/wms', {
-    layers: 'g_03_04:03_04_001_03_001_513_0000_00_00',
-    format: 'image/png',
-    opacity: 1,
-    transparent: true,
-    tiled: 'true'
-}).addTo(map);
+// var layer001 = L.tileLayer.wms('https://idesep.senamhi.gob.pe/geoserver/g_03_04/wms', {
+//     layers: 'g_03_04:03_04_001_03_001_513_0000_00_00',
+//     format: 'image/png',
+//     opacity: 1,
+//     transparent: true,
+//     tiled: 'true'
+// }).addTo(map);
 
 
 var groupOverLays = {
-    "Pronósticos": {
-        "UV 48 hrs.": layer001
+    "Layers": {
+        "Provincias": anotherLayer
     }
 };
 
